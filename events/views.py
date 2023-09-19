9173f0e40f4a8e79cd500f760621d989869b548e
jbozas <julianbozas@gmail.com>
2023-09-18 21:59:25 -0300

    deploying project

--- events/views.py
+++ events/views.py
@@ -2,4 +2,4 @@ from django.http import JsonResponse
 from .models import Event
 
 def event_list(request):
-    return JsonResponse([{event.get_data} for event in Event.objects.all()], safe=False)
+    return JsonResponse([event.get_data for event in Event.objects.all()], safe=False)
